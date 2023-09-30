6b61de0b646722c59bf75799c86eb2ee8f36432b
Ramonie <ramonmartinsmr@gmail.com>
2023-09-30 00:39:13 -0300

    Layout activity_main.xml e activity_salvar.xml

---
+++ app/src/main/java/ufrn/edu/meuslivros/salvar.kt
@@ -0,0 +1,11 @@
+package ufrn.edu.meuslivros
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class salvar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_salvar)
+    }
+}
